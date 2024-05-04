78457b39aedd53a2fb61ae9dcdaf678c717e729b
azzrf <fairuzizuan019@gmail.com>
2024-05-04 21:58:47 +0700

    add filter by price git add .without checked radiogit add .

--- search.php
+++ search.php
@@ -67,74 +67,59 @@ require_once 'service/db.php';
                         class="bg-secondary pr-3">Harga</span></h5>
                 <div class="bg-light p-4 mb-30">
                     <?php
-                    $prices = [];
+                    $price = "price BETWEEN 0 and 1000000000";
 
-                    if (isset($_GET['price-all'])) {
-                        $prices = [];
-                    } else {
-                        if (isset($_GET['price-1'])) {
-                            $prices[] = "price BETWEEN 0 AND 100000000";
-                        }
-                        if (isset($_GET['price-2'])) {
-                            $prices[] = "price BETWEEN 100000000 AND 200000000";
-                        }
-                        if (isset($_GET['price-3'])) {
-                            $prices[] = "price BETWEEN 200000000 AND 500000000";
-                        }
-                        if (isset($_GET['price-4'])) {
-                            $prices[] = "price BETWEEN 500000000 AND 1000000000";
-                        }
-                        if (isset($_GET['price-5'])) {
-                            $prices[] = "price >= 1000000000";
-                        }
-                    }
-
-                    $wherePrice = "";
-                    if (!empty($prices)) {
-                        $wherePrice = " AND " . implode(" OR ", $prices);
+                    if (isset($_GET['all-price'])) {
+                        $price;
+                    } elseif (isset($_GET['price-1'])) {
+                        $price = "price BETWEEN 0 AND 100000000";
+                    } elseif (isset($_GET['price-2'])) {
+                        $price = "price BETWEEN 100000000 AND 200000000";
+                    } elseif (isset($_GET['price-3'])) {
+                        $price = "price BETWEEN 200000000 AND 500000000";
+                    } elseif (isset($_GET['price-4'])) {
+                        $price = "price BETWEEN 500000000 AND 1000000000";
+                    } elseif (isset($_GET['price-5'])) {
+                        $price = "price >= 1000000000";
                     }
                     ?>
+
                     <form action="search.php" method="get" id="formFilterPrice">
-                        <div
-                            class="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
-                            <input type="checkbox" class="custom-control-input" id="price-all" name="price-all"
-                                <?= !$prices ? 'checked' : '' ?>>
-                            <label class="custom-control-label" for="price-all">Semua Harga</label>
-                            <!-- <span class="badge border font-weight-normal">99+</span> -->
+                        <div class="form-check d-flex align-items-center justify-content-between mb-3">
+                            <input class="form-check-input" type="radio" name="all-price" id="all-price"
+                                <?php $price === 'price BETWEEN 0 and 1000000000' ? 'checked' : '' ?>>
+                            <label class="form-check-label" for="all-price">
+                                All Price
+                            </label>
                         </div>
-                        <div
-                            class="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
-                            <input type="checkbox" class="custom-control-input" id="price-1" name="price-1" <?= $prices ? 'checked' : '' ?>>
-                            <label class="custom-control-label" for="price-1">Rp. 0 - Rp. 100jt</label>
-                            <!-- <span class="badge border font-weight-normal">15</span> -->
+                        <div class="form-check d-flex align-items-center justify-content-between mb-3">
+                            <input class="form-check-input" type="radio" name="price-1" id="price-1" <?php $price == 'price BETWEEN 0 AND 100000000' ? 'checked' : '' ?> >
+                            <label class="form-check-label" for="price-1">Rp0 - Rp100jt
+                            </label>
                         </div>
-                        <div
-                            class="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
-                            <input type="checkbox" class="custom-control-input" id="price-2" name="price-2" <?= $prices ? 'checked' : '' ?>>
-                            <label class="custom-control-label" for="price-2">Rp. 100jt - Rp. 200jt</label>
-                            <!-- <span class="badge border font-weight-normal">89</span> -->
+                        <div class="form-check d-flex align-items-center justify-content-between mb-3">
+                            <input class="form-check-input" type="radio" name="price-2" id="price-2" <?php $price == 'price BETWEEN 100000000 AND 200000000' ? 'checked' : '' ?>>
+                            <label class="form-check-label" for="price-2">Rp100jt - Rp200jt
+                            </label>
                         </div>
-                        <div
-                            class="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
-                            <input type="checkbox" class="custom-control-input" id="price-3" name="price-3" <?= $prices ? 'checked' : '' ?>>
-                            <label class="custom-control-label" for="price-3">Rp. 200jt - Rp. 500jt</label>
-                            <!-- <span class="badge border font-weight-normal">87</span> -->
+                        <div class="form-check d-flex align-items-center justify-content-between mb-3">
+                            <input class="form-check-input" type="radio" name="price-3" id="price-3" <?php $price == 'price BETWEEN 200000000 AND 500000000' ? 'checked' : '' ?>>
+                            <label class="form-check-label" for="price-3">Rp200jt - Rp500jt
+                            </label>
                         </div>
-                        <div
-                            class="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
-                            <input type="checkbox" class="custom-control-input" id="price-4" name="price-4" <?= $prices ? 'checked' : '' ?>>
-                            <label class="custom-control-label" for="price-4">Rp. 500jt - Rp. 1m</label>
-                            <!-- <span class="badge border font-weight-normal">46</span> -->
+                        <div class="form-check d-flex align-items-center justify-content-between mb-3">
+                            <input class="form-check-input" type="radio" name="price-4" id="price-4" <?php $price == 'price BETWEEN 500000000 AND 1000000000' ? 'checked' : '' ?>>
+                            <label class="form-check-label" for="price-4">Rp500jt - Rp1M
+                            </label>
                         </div>
-                        <div class="custom-control custom-checkbox d-flex align-items-center justify-content-between">
-                            <input type="checkbox" class="custom-control-input" id="price-5" name="price-5" <?= $prices ? 'checked' : '' ?>>
-                            <label class="custom-control-label" for="price-5">Rp. 1m+</label>
-                            <!-- <span class="badge border font-weight-normal">16</span> -->
+                        <div class="form-check d-flex align-items-center justify-content-between mb-3">
+                            <input class="form-check-input" type="radio" name="price-5" id="price-5" <?php $price == 'price >= 1000000000' ? 'checked' : '' ?>>
+                            <label class="form-check-label" for="price-5">> Rp1M
+                            </label>
                         </div>
                     </form>
                 </div>
                 <!-- Price End -->
-
             </div>
             <!-- Shop Sidebar End -->
 
@@ -166,32 +151,24 @@ require_once 'service/db.php';
                                         </option>
                                     </select>
                                 </form>
-
-                                <!-- <div class="btn-group ml-2">
-                                    <button type="button" class="btn btn-sm btn-light dropdown-toggle" data-toggle="dropdown">Showing</button>
-                                    <div class="dropdown-menu dropdown-menu-right">
-                                        <a class="dropdown-item" href="#">10</a>
-                                        <a class="dropdown-item" href="#">20</a>
-                                        <a class="dropdown-item" href="#">30</a>
-                                    </div>
-                                </div> -->
                             </div>
                         </div>
                     </div>
 
                     <?php
-                    $sqlSearchPosts = "SELECT * FROM posts WHERE title LIKE '%" . $_SESSION['query'] . "%'";
+                    $sqlSearchPosts = "SELECT * FROM posts WHERE title LIKE '%" . $_SESSION['query'] . "%' ";
 
                     $order = "";
 
                     if (isset($_GET['sortir'])) {
                         $order = " ORDER BY price " . $_GET['sortir'];
                     }
+
                     if (isset($_GET['sortir']) == 'default') {
                         $sqlSearchPosts;
                     }
 
-                    $sqlSearchPosts .= $wherePrice . $order;
+                    $sqlSearchPosts .= ' AND ' . $price . $order;
 
                     $queSearchPosts = $conn->query($sqlSearchPosts);
                     $searchPosts = $queSearchPosts->fetch_all(MYSQLI_ASSOC);
@@ -215,7 +192,6 @@ require_once 'service/db.php';
                                     <a class="h6 text-decoration-none text-truncate" href="#"><?= $search['title'] ?></a>
                                     <div class="d-flex align-items-center justify-content-center mt-2">
                                         <h5>Rp<?= number_format($search['price'], 0, ',', '.') ?></h5>
-                                        <!-- <h6 class="text-muted ml-2"><del>Rp. 200.000.000</del></h6> -->
                                     </div>
                                     <!-- // <div class="d-flex align-items-center justify-content-center mb-1">
                                     //     <small class="fa fa-star text-primary mr-1"></small>
@@ -263,8 +239,8 @@ require_once 'service/db.php';
         };
 
         // form filter harga
-        document.querySelectorAll('input[name^="price-"]').forEach(checkbox => {
-            checkbox.addEventListener('change', function () {
+        document.querySelectorAll('input[type=radio]').forEach(radio => {
+            radio.addEventListener('change', function () {
                 document.getElementById('formFilterPrice').submit()
             })
         });
