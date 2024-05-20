0b5de0c62fdfdcd5ca2f19d6b9262db495408689
root <root@robertho25.stembayo.com>
2024-05-20 05:50:28 +0000

    pema

--- detail.php
+++ detail.php
@@ -54,7 +54,7 @@ $data = $queDetail->fetch_assoc();
         <div class="row px-xl-5">
             <div class="col-12">
                 <nav class="breadcrumb bg-light mb-30">
-                    <a class="breadcrumb-item text-dark" href="/fe">Home</a>
+                    <a class="breadcrumb-item text-dark" href="index.php">Home</a>
                     <span class="breadcrumb-item active">Shop Detail</span>
                 </nav>
             </div>
--- service/db.copy.php
+++
@@ -1,8 +0,0 @@
-<?php
-$hostname = 'localhost';
-$username = 'root';
-$password = '';
-$dbName = 'adarumah';
-
-$conn = new mysqli($hostname, $username, $password, $dbName);
-
